de21177cf7e80e15facda0e0dcb675479e12ae85
unknown <twfarro@wpi.edu>
2026-01-07 23:38:53 -0500

    Emote integration with text

--- Assets/UI/ChatMessageDisplay.cs
+++ Assets/UI/ChatMessageDisplay.cs
@@ -26,27 +26,29 @@ public class ChatMessageDisplay : MonoBehaviour
         }
         foreach (ChatMessage.Fragment fragment in message.fragments)
         {
+
             if (fragment.type == ChatMessage.Fragment.Type.EMOTE)
             {
-                _text.text += "  ";
+                _text.text += "   ";
                 _text.ForceMeshUpdate();
-
+                float height = (_text.font.characterLookupTable['A'].glyph.metrics.height / _text.font.faceInfo.pointSize) * _text.fontSize;
                 TMPro.TMP_TextInfo textInfo = _text.textInfo;
-                int charIndex = textInfo.characterCount - 2;
+                int charIndex = textInfo.characterCount - 1;
                 // 2. Get character information
                 TMPro.TMP_CharacterInfo charInfo = textInfo.characterInfo[charIndex];
                 // 3. Calculate the center of the character in local space
                 AnimatedTextureDisplay child = Instantiate(_imgPrefab);
                 child.transform.SetParent(_text.transform);
                 // child.transform.sizeDelta = new Vector2(_text.fontSize, _text.fontSize) * 0.75f;
-                Vector3 centerLocal = ((Vector2)charInfo.bottomLeft + (Vector2)charInfo.topRight) / 2;
+                Vector3 centerLocal = ((Vector2)charInfo.bottomLeft + (Vector2)charInfo.topRight + new Vector2(0, height)) / 2;
                 child.transform.localPosition = centerLocal;
+                child.transform.localScale = Vector3.one * _text.fontSize;
                 child.DisplayTexture(fragment.image);
                 _emotes.Add(child);
             }
             else
             {
-                _text.text += fragment.text;
+                _text.text += fragment.text.Trim();
             }
         }
     }
