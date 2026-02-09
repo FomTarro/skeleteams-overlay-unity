971897655f30308ad7ec9f0c5b97036eb4068f26
unknown <twfarro@wpi.edu>
2026-02-08 19:37:02 -0500

    more layouting

--- Assets/UI/LayoutManager.cs
+++ Assets/UI/LayoutManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Klak.Spout;
 using Skeletom.BattleStation.Server;
 using UnityEngine;
 
@@ -9,10 +10,28 @@ public class LayoutManager : MonoBehaviour
     [SerializeField]
     private WebServer _webServer;
 
+    [SerializeField]
+    private GameObject _collabBar;
+    [SerializeField]
+    private SpoutReceiver _mainWindow;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // _webServer.RegisterEndpoint(new Endpoint("/camera/toggle", (req) =>
+        // {
+        //     if (_mainWindow.sourceName.Equals(_facecamSpoutName))
+        //     {
+        //         _collabBar.SetActive(true);
+        //         _mainWindow.sourceName = _captureSpoutName;
+        //     }
+        //     else
+        //     {
+        //         _collabBar.SetActive(false);
+        //         _mainWindow.sourceName = _facecamSpoutName;
+        //     }
+        //     return new EndpointResponse(200, "");
+        // }));
     }
 
     // Update is called once per frame
